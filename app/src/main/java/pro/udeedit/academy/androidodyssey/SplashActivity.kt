c0e229a2a7f4fd1a49e8c837d27bd9eb12350f10
Sargius <Dr.Sargius@gmail.com>
2026-07-23 14:05:18 +0400

    - feat: AO-13 add basic SplashActivity launcher forwarding to MainActivity

--- app/src/main/java/pro/udeedit/academy/androidodyssey/SplashActivity.kt
+++ app/src/main/java/pro/udeedit/academy/androidodyssey/SplashActivity.kt
@@ -1,48 +1,16 @@
 package pro.udeedit.academy.androidodyssey
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import pro.udeedit.academy.androidodyssey.ui.theme.AndroidOdysseyTheme
 
 class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AndroidOdysseyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidOdysseyTheme {
-        Greeting("Android")
+        // Immediately navigate to the main host activity and close splash.
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
